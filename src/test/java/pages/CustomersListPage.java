47aacfbd89b96a91a982586695e45f86fe8a6519
pligina-e <82607076+pligina-e@users.noreply.github.com>
2023-10-02 17:29:58 +0300

    Update CustomersListPage.java

--- src/test/java/pages/CustomersListPage.java
+++ src/test/java/pages/CustomersListPage.java
@@ -5,15 +5,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
-import static helpers.Wait.waitThenClick;
-import static helpers.Wait.waitUntilVisibleAllElements;
-import static helpers.Wait.writeText;
+import static helpers.Wait.*;
 
 /**
  * Класс для работы с формой просмотра таблицы клиентов (Второй, третий тест-кейс)
@@ -30,14 +24,17 @@ public class CustomersListPage extends BasePage {
     WebElement searchCustomerField;
     @FindBy(xpath = "//*[@class= 'ng-binding']")
     List<WebElement> threeTableColumns;
+
     public CustomersListPage(final WebDriver webDriver) {
         super(webDriver);
     }
+
     @Step("Wait and press the button to add customer")
     public CustomersListPage pressButton() {
         waitThenClick(driver, customersButton);
         return this;
     }
+
     /**
      * Метод для нажатия на содержимое первой ячейки таблицы (для того, чтобы произошла сортировка)
      */
@@ -46,6 +43,7 @@ public class CustomersListPage extends BasePage {
         waitThenClick(driver, firstNameField);
         return this;
     }
+
     /**
      * Метод для запоминания всех значений первого столбца таблицы
      * @return список имён
@@ -54,22 +52,25 @@ public class CustomersListPage extends BasePage {
     public ArrayList<String> listNames() {
         waitUntilVisibleAllElements(driver, firstNameTableCells);
         ArrayList<String> listText = new ArrayList<>();
-        firstNameTableCells.stream()
-                .forEach(name->listText.add(name.getText()));
+        firstNameTableCells.stream().forEach(name -> listText.add(name.getText()));
+        //ArrayList listText = (ArrayList) firstNameTableCells.stream().collect(Collectors.toList());
+
+        //ArrayList listText2 = firstNameTableCells.stream().map(WebElement::getText).toArray(ArrayList::new);
+        //ArrayList listText3 = firstNameTableCells.stream().toArray(n->n.getText());
         return listText;
     }
+
     /**
      * Метод для сортировки списка имен в обратном алфавитном порядке
-     * @see #listNames()
      * @return список имён в обратном алфавитном порядке
      */
     @Step("Remember all names sorted in reverse order")
-    public ArrayList<String> listNamesReverseOrder() {
-        ArrayList<String> listText = listNames();
+    public ArrayList<String> listNamesReverseOrder(ArrayList<String> listText) {
         Comparator<String> reverseComparator = Comparator.reverseOrder();
         Collections.sort(listText, reverseComparator);
         return listText;
     }
+
     /**
      * Метод для запоминания значений трёх первых столбцов таблицы (необходимо для поиска клиента)
      * @return все значения из трёх первых столбцов
@@ -78,34 +79,31 @@ public class CustomersListPage extends BasePage {
     public ArrayList<String> threeColumnWordList() {
         waitUntilVisibleAllElements(driver, threeTableColumns);
         ArrayList<String> listText = new ArrayList<>();
-        threeTableColumns.stream()
-                .forEach(values->listText.add(values.getText()));
+        threeTableColumns.stream().forEach(values -> listText.add(values.getText()));
         return listText;
     }
+
     /**
      * Метод для случайного выбора слова из списка значений трёх первых столбцов и его ввода в поле (поиск клиента)
-     * @see #threeColumnWordList()
      * @return слово
      */
     @Step("Select a random word from the list and write it in the field")
-    public String randomWordFromTable() {
+    public String randomWordFromTable(ArrayList<String> listText) {
         Random random = new Random();
-        ArrayList<String> listText = threeColumnWordList();
         String randomWord = listText.get(random.nextInt(listText.size()));
-        writeText(driver, searchCustomerField, randomWord);
+        inputText(driver, searchCustomerField, randomWord);
         return randomWord;
     }
+
     /**
      * Метод для проверки того, остались ли в табл. строки, содержащие введённое слово в поле "Search Customer" (слово выбрано случайным образом)
-     * @see #randomWordFromTable()
      * @return булевское значение
      */
     @Step("Check the remaining rows for the content of the entered random word in the field")
-    public boolean checkWordInTable() {
-        String customerSearchWord = randomWordFromTable();
+    public boolean checkWordInTable(String customerSearchWord) {
         boolean containsWord = false;
         waitUntilVisibleAllElements(driver, threeTableColumns);
-        containsWord = threeTableColumns.stream().anyMatch(r->r.getText().contains(customerSearchWord));
+        containsWord = threeTableColumns.stream().anyMatch(r -> r.getText().contains(customerSearchWord));
         return containsWord;
     }
 }
